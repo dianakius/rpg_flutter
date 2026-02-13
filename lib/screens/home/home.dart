f4f244572f806d3eac49bc1f1bc61f5d276db2d7
dianakius <eingidiana@gmail.com>
2026-02-13 15:29:30 +0200

    feat: output CharacterCard widget fo each character

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -23,7 +23,7 @@ class _HomeState extends State<Home> {
               child: ListView.builder(
                 itemCount: characters.length,
                 itemBuilder: (_, index) {
-                  return CharacterCard();
+                  return CharacterCard(character: characters[index]);
                 },
               ),
             ),
