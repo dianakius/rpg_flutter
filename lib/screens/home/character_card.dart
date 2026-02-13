2d3b7e67bf86c342f0812fa4cbca47bf8d1cc389
dianakius <eingidiana@gmail.com>
2026-02-13 15:28:36 +0200

    feat: input an argument (required this.character

--- lib/screens/home/character_card.dart
+++ lib/screens/home/character_card.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CharacterCard extends StatelessWidget {
-  const CharacterCard({super.key});
+  const CharacterCard({required this.character, super.key});
+
+  final String character;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Row(children: [Text(character)]),
+      ),
+    );
   }
 }
