031607b2b369a243450bb0a0c0ceef2bbc277da3
dianakius <eingidiana@gmail.com>
2026-02-13 16:55:06 +0200

    feat: map and list

--- lib/models/stats.dart
+++ lib/models/stats.dart
@@ -1,9 +1,28 @@
+// import 'package:flutter_rpg/models/character.dart';
+
 mixin Stats {
   int _points = 10;
   int _health = 10;
   int _attack = 10;
   int _defense = 10;
   int _skill = 10;
+
+int get points  => _points;
+
+Map<String,int> get statsAsMap => {
+  "health": _health, 
+  "attack": _attack,
+  "defense": _defense,
+  "skill": _skill,
+};
+
+List<Map<String, String>> get statsAsFormattedList => [
+ {'title': 'health', 'value': _health.toString()},
+ {'title': 'attack', 'value': _attack.toString()},
+ {'title': 'defense', 'value': _defense.toString()},
+ {'title': 'skill', 'value': _skill.toString()},
+];
+
 
   void increaseStat(String stat) {
     if (_points > 0) {
@@ -41,3 +60,5 @@ mixin Stats {
     }
   }
 }
+
+
